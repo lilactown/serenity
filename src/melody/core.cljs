e69166db5f4063395027151c7fbbe4b9900b884e
Will Acton <will@willacton.com>
2020-06-09 14:51:09 -0700

    rewrite

--- src/melody/core.cljs
+++ src/melody/core.cljs
@@ -1,117 +1,183 @@
 (ns melody.core
   (:require [lilactown.harmony :as harmony]
-            [clojure.set :as set]))
+            [lilactown.dynamic-scope :as ds]))
 
 
-(defprotocol IAgent
-  (-send [agent f]))
+(extend-protocol IPrintWithWriter
+  js/Set
+  (-pr-writer [this writer opts]
+    (pr-sequential-writer
+     writer pr-writer "#object[Set #{" " " "}]"
+     opts this)))
+
+
+(defprotocol ISource
+  (-send [src x]))
 
 
 (defprotocol IReactive
-  (-add-dep [this r])
-  (-remove-dep [this r]))
+  (-add-edge [a b])
+  (-remove-edge [a b]))
 
 
-(defprotocol ITrack
-  (-trigger [this]))
+(defprotocol INode
+  (-order [node])
+  (-set-order [node n])
+  (-calculate [node]))
 
 
-(def ^:dynamic *reactive-context* nil)
+(def ^:dynamic *reactive-context*)
 
 
-(def ^:dynamic *current-tracker* nil)
+(def ^:dynamic *transaction*)
 
 
-(deftype Agent [ref ^:mutable dependents]
-  IAgent
-  (-send [this f]
+(defn- ordered-set
+  [coll]
+  (apply sorted-set-by (comp < #(-order %)) coll))
+
+
+(defn- calculate-all-nodes!
+  [initial-edges]
+  (loop [edges (ordered-set initial-edges)
+         n 10]
+    (when-some [edge (first edges)]
+      (when (> n 0)
+        (prn edge edges)
+        (recur (into (disj edges edge)
+                     (-calculate edge))
+               (dec n))))))
+
+
+(deftype Source [ref reducer edges]
+  IDeref
+  (-deref [this]
+    (when (some? *reactive-context*)
+      ;; `js/Set`
+      (.add *reactive-context* this))
+    (harmony/deref ref))
+
+  ISource
+  (-send [_ x]
     (doto (harmony/branch)
-      (.add #(harmony/alter ref f))
-      ;; TODO schedule
-      (.add #(doseq [dep dependents]
-               (-trigger dep)))
-      (.flush)
-      (.commit)))
+      ;; calculate new state of source
+      (.add #(harmony/alter
+              ref
+              (fn [current]
+                (reducer current x))))
+      ;; calculate nodes
+      (.add #(calculate-all-nodes! edges))
+      (.commit))
+    nil)
 
   IReactive
-  (-remove-dep [this r]
-    (.filter dependents #(not= r %)))
+  (-add-edge [_ node]
+    (.add edges node))
+  (-remove-edge [_ node]
+    (.delete edges node))
 
+  INode
+  (-order [_] 0)
+  ;; TODO these aren't used
+  (-set-order [_ _] 0)
+  (-calculate [_] nil))
+
+
+(defn- set-difference
+  [s1 s2]
+  (->> s1
+       (filter #(not (.has s2 %)))
+       (js/Set.)))
+
+
+(defn- set-intersection
+  [s1 s2]
+  (-> s1
+      (filter #(.has s2 %))
+      (js/Set.)))
+
+
+;; `edges` are things that depend on me
+(deftype Node [ref f xf edges ^:mutable derefs ^:mutable order]
   IDeref
   (-deref [this]
-    (when *reactive-context*
-      (.push dependents *current-tracker*)
-      (.push *reactive-context* this))
-    (harmony/deref ref)))
+    (when (some? *reactive-context*)
+      (.add *reactive-context* this))
+    (harmony/deref ref))
+
+  INode
+  (-order [_] order)
+  (-set-order [_ n]
+    (when (> n order)
+      (set! order n)))
+  (-calculate [this]
+    (let [derefs' (js/Set.)]
+      ;; run `f` with `*reactive-context*` set so that we can diff our derefs
+      ;; and clean up any that have become stale
+      (binding [*reactive-context* derefs']
+        ;; TODO run `xf` for transducing
+        (harmony/set ref (f)))
+
+      ;;
+      ;; TODO this all needs to _after_ the transaction has committed.
+      ;;
+
+      ;; remove ourself from derefs that are stale
+      (doseq [deref (set-difference derefs derefs')]
+        (-remove-edge deref this))
+
+      ;; TODO only do this for difference the other way maybe?
+      (doseq [deref derefs']
+        (-add-edge deref this))
+
+      ;; set current derefs
+      (set! derefs derefs')
 
 
-(deftype Tracker [f ^:mutable ref dependents ^:mutable depends-on]
-  ITrack
-  (-trigger [this]
-    ;; assume we're in a transaction
-    (binding [*reactive-context* #js []
-              *current-tracker* this]
-      (harmony/alter ref f)
-      ;; TODO defer setting of depends-on until after
-      (let [depends-on' (set *reactive-context*)]
-        (doseq [dep (set/difference depends-on depends-on')]
-          (prn "n")
-          (-remove-dep dep this))
-        (set! depends-on depends-on')))
-    (doseq [dep dependents]
-      ;; TODO schedule
-      (-trigger dep)))
+      ;; TODO set order
 
-  IReactive
-  (-add-dep [this r]
-    (.push depends-on r))
-
-  IDeref
-  (-deref [this]
-    (when *reactive-context*
-      ;; defer this
-      (.push dependents *current-tracker*))
-    (if (some? ref)
-      (harmony/deref ref)
-      ;; initialize
-      (binding [*reactive-context* #js []
-                *current-tracker* this]
-        (set! ref (harmony/ref (f)))
-        (set! depends-on (set *reactive-context*))
-        (harmony/deref ref)))))
+      ;; return edges to be calculated
+      edges)))
 
 
-(defn agent
-  [x]
-  (->Agent (harmony/ref x) #js []))
+(defn source
+  [reducer default]
+  (->Source (harmony/ref default) reducer (js/Set.)))
 
 
-(defn send
-  [agent f]
-  (-send agent f)
-  nil)
-
-
-(defn track*
+(defn node
   [f]
-  (->Tracker f nil #js [] #js []))
+  (let [node (->Node
+              (harmony/ref nil)
+              f
+              nil ;; `xf`
+              (js/Set.) ;; `edges`
+              (js/Set.) ;; `derefs`
+              ;; assume at least order 11
+              1)]
+    ;; run first calculation eagerly
+    (-> (harmony/branch)
+        (.add #(-calculate node))
+        (.commit))
+    node))
+
+
+(defn send [src message]
+  (-send src message))
 
 
 (comment
-  (def foo (agent 0))
+  (def a (source #(case %2
+                    :inc (inc %1)
+                    :dec (dec %1)) 0))
 
+  (def b (node #(inc @a)))
 
-  @foo
+  (def c (node #(+ @a @b)))
 
+  (send a :dec)
 
-  (def bar (track* #(deref foo)))
+  @a
 
-
-  @bar
-
-  (.-dependents foo)
-
-  (.-depends-on bar)
-
-  (send foo inc)
-  )
+  @b
+)
